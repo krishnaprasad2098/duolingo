f546bb84f9c1038f726da753b0a7ed909e80a979
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-04-18 22:43:01 +0530

    add header

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -13,8 +13,8 @@ import { Button } from "@/components/ui/button";
 const Header = () => {
   return (
     <div>
-      <div className="flex justify-between">
-        <div className="px-4 py-3 flex items-center justify-start gap-2">
+      <div className="flex py-3 justify-between px-9">
+        <div className=" flex items-center justify-start gap-2">
           <Image
             src="./mascot.svg"
             width={40}
