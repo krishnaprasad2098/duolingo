040d59d73deb3dc89c104379e3fda5e263ac4d80
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-05-08 20:59:05 +0530

    fixed styling of layout(marketing) in lg,md,screen sizes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -6,9 +6,9 @@ type Props = {
 };
 export default function MarketingLayout({ children }: Props) {
   return (
-    <div className="flex flex-col min-h-screen ">
+    <div className="flex  flex-col min-h-screen ">
       <Header />
-      <main className="flex flex-col flex-1 items-center justify-center ">
+      <main className="flex flex-col lg:m-0 my-6 lg:flex-1 items-center justify-center ">
         {children}
       </main>
       <Footer />
