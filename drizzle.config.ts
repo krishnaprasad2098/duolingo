aafb257ddeecead6d26c0369cbb77ebe917abe60
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-05-15 16:10:53 +0530

    migrated drizzle from 0.20.17 to 0.21.2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,11 @@ import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
+  dialect: "postgresql",
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  // driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
