4a45269e72cf3ceed800d530b6a7f556abd1e526
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-05-16 12:30:35 +0530

    add lesson layout and lesson page

---
+++ app/lesson/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const LessonLayout = ({ children }: Props) => {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex h-full w-full flex-col">{children}</div>
+    </div>
+  );
+};
+
+export default LessonLayout;
