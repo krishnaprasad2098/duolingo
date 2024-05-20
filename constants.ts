1223eebe5837de99f6dba65bba59aa8760a4877d
krishnaprasad2098 <payamma.krishna@gmail.com>
2024-05-20 21:43:37 +0530

    add constant values

---
+++ constants.ts
@@ -0,0 +1,28 @@
+export const POINTS_TO_REFILL = 10;
+
+export const quests = [
+  {
+    title: "Earn 20 XP",
+    value: 20,
+  },
+  {
+    title: "Earn 40 XP",
+    value: 40,
+  },
+  {
+    title: "Earn 60 XP",
+    value: 60,
+  },
+  {
+    title: "Earn 80 XP",
+    value: 80,
+  },
+  {
+    title: "Earn 100 XP",
+    value: 100,
+  },
+  {
+    title: "Earn 120 XP",
+    value: 120,
+  },
+];
